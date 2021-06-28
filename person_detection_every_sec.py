be45be2f3220f723287fbaca4000db6709de010f
Dmitry Ioksha <41271764+DmitryIo@users.noreply.github.com>
2021-06-28 19:14:50 +0300

    Update person_detection_every_sec.py

--- person_detection_every_sec.py
+++ person_detection_every_sec.py
@@ -61,8 +61,8 @@ if not os.path.exists(MODEL_FILE):
 
 detection_graph = tf.Graph()
 with detection_graph.as_default():
-    od_graph_def = tf.GraphDef()
-    with tf.gfile.GFile(PATH_TO_CKPT, 'rb') as fid:
+    od_graph_def = tf.compat.v1.GraphDef()
+    with tf.compat.v2.io.gfile.GFile(PATH_TO_CKPT, 'rb') as fid:
         serialized_graph = fid.read()
         od_graph_def.ParseFromString(serialized_graph)
         tf.import_graph_def(od_graph_def, name='')
